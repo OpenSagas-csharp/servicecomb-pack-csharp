f9fd953bbe4380d5ddea67bff89617f2633566f2
WithLin <248628578@qq.com>
2018-08-07 16:19:55 +0800

    add compensation handler

---
+++ src/Servicecomb.Saga.Omega.Core/Transaction/Impl/CompensationMessageHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Servicecomb.Saga.Omega.Core.Context;
+
+namespace Servicecomb.Saga.Omega.Core.Transaction.Impl
+{
+    public class CompensationMessageHandler: IMessageHandler
+    {
+        private readonly IMessageSender _sender;
+
+        private readonly CompensationContext _compensationContext;
+
+        public CompensationMessageHandler(IMessageSender sender, CompensationContext context)
+        {
+            _sender = sender;
+            _compensationContext = context;
+        }
+
+        public void OnReceive(string globalTxId, string localTxId, string parentTxId, string compensationMethod,
+            params object[] payloads)
+        {
+            _compensationContext.Apply(globalTxId, localTxId, compensationMethod, payloads);
+            _sender.Send(new TxCompensatedEvent(globalTxId, localTxId, parentTxId, compensationMethod));
+        }
+    }
+}
