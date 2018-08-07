ce461137f4aa912667869b72a4497dcee4f35d83
WithLin <248628578@qq.com>
2018-08-07 16:19:03 +0800

    fix add MessageSerializer interface

---
+++ src/Servicecomb.Saga.Omega.Abstractions/Transaction/IMessageSerializer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Servicecomb.Saga.Omega.Abstractions.Transaction
+{
+    public interface IMessageSerializer
+    {
+        byte[] Serialize(Object[] objects);
+    }
+}
