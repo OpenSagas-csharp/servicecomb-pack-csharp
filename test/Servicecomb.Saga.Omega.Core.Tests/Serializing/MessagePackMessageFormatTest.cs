4f64236d8a3a8e983e637703c9c959515af5db62
WithLin <withlin@yeah.net>
2018-08-11 23:38:35 +0800

    add Serializing module test

---
+++ test/Servicecomb.Saga.Omega.Core.Tests/Serializing/MessagePackMessageFormatTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Servicecomb.Saga.Omega.Core.Serializing;
+using Xunit;
+
+namespace Servicecomb.Saga.Omega.Core.Tests.Serializing
+{
+    public class MessagePackMessageFormatTest
+    {
+        [Fact]
+        public void CanFormat()
+        {
+            var messageFormat=new MessagePackMessageFormat();
+            Assert.NotEmpty(messageFormat.Serialize(new object[] { 123 }));
+            Assert.NotEmpty( messageFormat.Deserialize<object[]>(messageFormat.Serialize(new object[] { 123 })));
+
+        }
+    }
+}
