dcd472b1b51cf18cc0a98fcc091d75d9c4686634
WithLin <withlin@yeah.net>
2018-08-13 23:24:49 +0800

    fix add ServiceLocator

---
+++ src/Servicecomb.Saga.Omega.Core/Transaction/ServiceLocator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Servicecomb.Saga.Omega.Core.Transaction
+{
+    public class ServiceLocator
+    {
+        private ServiceProvider _currentServiceProvider;
+        private static ServiceProvider _serviceProvider;
+
+        public ServiceLocator(ServiceProvider currentServiceProvider)
+        {
+            _currentServiceProvider = currentServiceProvider;
+        }
+
+        public static ServiceLocator Current
+        {
+            get
+            {
+                return new ServiceLocator(_serviceProvider);
+            }
+        }
+
+        public static void SetLocatorProvider(ServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public object GetInstance(Type serviceType)
+        {
+            return _currentServiceProvider.GetService(serviceType);
+        }
+
+        public TService GetInstance<TService>()
+        {
+            return _currentServiceProvider.GetService<TService>();
+        }
+    }
+}
