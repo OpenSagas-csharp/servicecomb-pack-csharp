1ad37db5db0996f782de837205ec918b3bf3b1f0
WithLin96 <248628578@qq.com>
2018-08-13 11:17:09 +0800

    add diagnostic module

---
+++ src/Servicecomb.Saga.Omega.Core/Utils/EnumerableExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Servicecomb.Saga.Omega.Core.Utils
+{
+    internal static class EnumerableExtensions
+    {
+        public static IEnumerable<T> Distinct<T, TK>(this IEnumerable<T> source, Func<T, TK> predicate)
+        {
+            var sets = new HashSet<TK>();
+            foreach (var item in source)
+            {
+                if (sets.Add(predicate(item)))
+                {
+                    yield return item;
+                }
+            }
+        }
+    }
+}
