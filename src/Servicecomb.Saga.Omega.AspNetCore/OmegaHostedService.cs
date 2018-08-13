e821db1d7feadc8b91b4f9b32062655c6569c371
WithLin <248628578@qq.com>
2018-08-13 17:30:55 +0800

    add licensed

--- src/Servicecomb.Saga.Omega.AspNetCore/OmegaHostedService.cs
+++ src/Servicecomb.Saga.Omega.AspNetCore/OmegaHostedService.cs
@@ -1,7 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
+﻿/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 using System.Diagnostics;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Grpc.Core;
@@ -11,9 +25,7 @@ using Servicecomb.Saga.Omega.Abstractions.Logging;
 using Servicecomb.Saga.Omega.Abstractions.Transaction;
 using Servicecomb.Saga.Omega.Core.Connector.GRPC;
 using Servicecomb.Saga.Omega.Core.Diagnostics;
-using Servicecomb.Saga.Omega.Core.Logging;
 using Servicecomb.Saga.Omega.Core.Transaction;
-using Servicecomb.Saga.Omega.Core.Transaction.Impl;
 using Servicecomb.Saga.Omega.Protocol;
 
 namespace Servicecomb.Saga.Omega.AspNetCore
