385a17479aa913a7e4320645392e2a7ba1d0eed2
WithLin <withlin@yeah.net>
2018-08-24 14:48:13 +0800

    fix  code style

--- samples/Omega.Sample.Booking/Controllers/ValuesController.cs
+++ samples/Omega.Sample.Booking/Controllers/ValuesController.cs
@@ -16,7 +16,7 @@ namespace Omega.Sample.Booking.Controllers
             //return new string[] { "value1", "value2" };
             var httpClient = new HttpClient();
             httpClient.GetAsync("http://localhost:5002/api/values");
-          var content=  httpClient.GetAsync("http://localhost:5003/api/values");
+            var content=  httpClient.GetAsync("http://localhost:5003/api/values");
             Thread.Sleep(5000);
             throw  new NullReferenceException();
             return "";
