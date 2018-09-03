26cdfd127795f7755d4482be6bfa9c2c1b0312b2
WithLin <withlin@yeah.net>
2018-09-03 17:16:06 +0800

    add intercept

---
+++ src/Servicecomb.Saga.Omega.Core/Diagnostics/DiagnosticListenerObserver.cs
@@ -0,0 +1,45 @@
+﻿//FROMhttps://github.com/dotnet/corefx/blob/master/src/System.Diagnostics.DiagnosticSource/src/DiagnosticSourceUsersGuide.md
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using Servicecomb.Saga.Omega.Abstractions.Diagnostics;
+using Servicecomb.Saga.Omega.Core.Utils;
+
+namespace Servicecomb.Saga.Omega.Core.Diagnostics
+{
+    public class DiagnosticListenerObserver: IObserver<DiagnosticListener>
+    {
+        private readonly IEnumerable<IDiagnosticItercept> _diagnosticItercepts;
+        private static readonly object _object=new object();
+
+        public DiagnosticListenerObserver(IEnumerable<IDiagnosticItercept> diagnosticItercepts)
+        {
+            _diagnosticItercepts = diagnosticItercepts;
+        }
+
+        public void OnCompleted()
+        {
+            // just do nothing here
+        }
+
+        public void OnError(Exception error)
+        {
+            // just do nothing here
+        }
+
+        public void OnNext(DiagnosticListener diagnosticListener)
+        {
+            var result = _diagnosticItercepts.Distinct(x => x.ListenerName).ToList()
+                .Find(x => x.ListenerName == diagnosticListener.Name);
+            if (result == null) return;
+            //The DiagnosticSource/DiagnosticListener code is thread safe, but the callback code also needs to be thread safe.
+            lock (_object)
+            {
+                diagnosticListener.Subscribe(new DiagnosticObserverIntercept(result));
+            }
+        }
+
+    }
+}
---
+++ src/Servicecomb.Saga.Omega.Core/Diagnostics/DiagnosticObserverIntercept.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Microsoft.Extensions.DiagnosticAdapter;
+using Servicecomb.Saga.Omega.Abstractions.Diagnostics;
+
+namespace Servicecomb.Saga.Omega.Core.Diagnostics
+{
+    public class DiagnosticObserverIntercept : IObserver<KeyValuePair<string, object>>
+    {
+        private readonly IDiagnosticItercept _diagnosticItercept;
+        private readonly Dictionary<string, MethodInfo> _dictionary;
+        private readonly Dictionary<string, string> _diagnosticNameDictionary = new Dictionary<string, string>()
+        {
+            { "Microsoft.AspNetCore.Hosting.BeginRequest","httpContext"},
+            {"System.Net.Http.Request", "Request"},
+        };
+        private static readonly object _object = new object();
+        public DiagnosticObserverIntercept(IDiagnosticItercept diagnosticItercept)
+        {
+            _diagnosticItercept = diagnosticItercept;
+            _dictionary = new Dictionary<string, MethodInfo>();
+            Inint();
+
+
+        }
+
+        public void OnCompleted()
+        {
+            // just do nothing here
+        }
+
+        public void OnError(Exception error)
+        {
+            // just do nothing here
+        }
+
+        public void OnNext(KeyValuePair<string, object> value)
+        {
+            lock (_object)
+            {
+                if (!_dictionary.ContainsKey(value.Key)) return;
+                var method = _dictionary.First(x => x.Key == value.Key).Value;
+                var property = value.Value.GetType().GetProperty(_diagnosticNameDictionary[value.Key]);
+                object[] arg = { property?.GetValue(value.Value) };
+                method.Invoke(_diagnosticItercept, arg);
+            }
+            
+        }
+
+        void Inint()
+        {
+
+
+            var assembly = Assembly.GetCallingAssembly();
+            var methodInfos = assembly.GetTypes()
+                .SelectMany(t => t.GetMethods())
+                .Where(m => m.GetCustomAttributes(typeof(DiagnosticNameAttribute), false).Length > 0)
+                .ToList();
+            foreach (var method in methodInfos)
+            {
+                var attribute = method.GetCustomAttribute<DiagnosticNameAttribute>();
+                if (attribute == null) continue;
+                _dictionary.Add(attribute.Name, method);
+
+            }
+        }
+    }
+}
